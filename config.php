7fcf070682ec2c209d62981f3de310978d3fc37d
JOEAI <42526993+jomarbp@users.noreply.github.com>
2025-09-16 09:06:40 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $clave = "";
+    try{
+        $conexion = new PDO("mysql:host=$servidor;dbname=bd_clientes", $usuario, $clave);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexión exitosa";
+    }catch(PDOException $e)
+    {
+         echo "Error en la conexión: " . $e->getMessage();
+    }
+?>
